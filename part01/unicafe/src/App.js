aa535de5a0cec389988d938f0a54de613bf8cfde
Hazem Hassine <hazemhassine.edu@gmail.com>
2021-12-29 13:08:03 +0100

    finished unicafe

--- part01/unicafe/src/App.js
+++ part01/unicafe/src/App.js
@@ -1,30 +1,71 @@
 import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-const Button = (props) => {
-  const text = props.text;
-  return (<button>{text}</button>);
-};
-const Display = (props) => {
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+const StatisticLine = ({ text, value }) => {
   return (
-    <p>{props.text} {props.value}</p>
-  )
-}
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
-  const setToGood = (value) => {
-   setGood(value++)
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
+const Statistics = (props) => {
+  // caching the prop
+  const reviews = props.reviews;
+  const all = reviews.good + reviews.bad + reviews.neutral;
+  let positive = (all == 0)?0:parseFloat((reviews.good / all ) * 100).toPrecision(2);
+  if (reviews.good != 0 & reviews.neutral == 0 & reviews.bad == 0) {
+    positive = 100;
+  }
+  let average;
+  if (all!=0) {
+    average = (-reviews.bad+ reviews.good)/all;
+  }
+  else {
+    average = 0;
   }
   return (
+    <table id="statistics">
+      <tbody>
+        <StatisticLine text="Good" value={reviews.good} />
+        <StatisticLine text="Neutral" value={reviews.neutral} />
+        <StatisticLine text="Bad" value={reviews.bad} />
+        <StatisticLine text="All" value={all} />
+        <StatisticLine text="Average" value={average.toPrecision(2)} />
+        <StatisticLine text="Positive" value={positive.toString() + ' %'} />
+      </tbody>
+    </table>
+  );
+};
+
+const App = () => {
+  const [review, setReview] = useState(
+    // intial state
+    {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const good = () => {
+    setReview({ ...review, good: review.good + 1 });
+  };
+  const neutral = () => {
+    setReview({ ...review, neutral: review.neutral + 1 });
+  };
+  const bad = () => {
+    setReview({ ...review, bad: review.bad + 1 });
+  };
+
+  return (
     <div>
-      <h1>Give Feedback</h1>
-      <Button onClick={setToGood(good)} text="bad"/>
-      <Button onClick={setToGood(good)} text="neutral"/>
-      <Button onClick={setToGood(good)} text="good"/>
-      <Display text="good" value={good}/>
+      <h1>Give feedback</h1>
+      <Button onClick={good} text="Good" />
+      <Button onClick={neutral} text="Neutral" />
+      <Button onClick={bad} text="Bad" />
+      <h1>Statistics</h1>
+      <Statistics reviews={review} />
     </div>
   );
 };
